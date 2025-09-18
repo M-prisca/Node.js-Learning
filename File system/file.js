b9cdd3c9ee871db3a9b83f5e69216d00733c395c
Prisca <priscamasereli@gmail.com>
2025-09-18 10:20:01 +0200

    Feat: Directories deleted if it exists

--- File system/file.js
+++ File system/file.js
@@ -23,10 +23,18 @@ fs.writeFile("./blog1.txt", "Hello again", () => {
 });
 
 // directories
-
-fs.mkdir("../assets", (err) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log("folder created");
-});
+if (!fs.existsSync("../assets")) {
+  fs.mkdir("../assets", (err) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log("folder created");
+  });
+} else {
+  fs.rmdir("../assets", (err) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log("file deleted");
+  });
+}
