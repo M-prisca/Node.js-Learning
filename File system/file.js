58743f7f3e349f92b7b418883bc05646c8beaba0
Prisca <priscamasereli@gmail.com>
2025-09-18 10:05:10 +0200

    Feat: reading and Writing files

---
+++ File system/file.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+
+// Reading the files
+fs.readFile("./blog.txt", (err, data) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(data.toString());
+});
+//console.log("last line"); last line
+// Hello, Prisca!
+
+// Writing files
+
+fs.writeFile("./blog.txt", "Hello world", () => {
+  console.log("file was written");
+});
+
+// create a file if it doesn't exist by writing on it
+
+fs.writeFile("./blog1.txt", "Hello again", () => {
+  console.log("file was written");
+});
