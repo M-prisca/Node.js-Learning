15d5c8ef67eebfc1a0451597ae81deee6ebe8e76
Prisca <priscamasereli@gmail.com>
2025-10-01 09:25:23 +0200

    Feat: Streams with encoding param

--- Streams/streams.js
+++ Streams/streams.js
@@ -1,7 +1,9 @@
 const fs = require("fs");
-const readStream = fs.createReadStream("../File system/blog.txt");
+const readStream = fs.createReadStream("../File system/blog.txt", {
+  encoding: "utf8",
+});
 
 readStream.on("data", (chunk) => {
   console.log(".....NEW CHUNK.....");
-  console.log(chunk.toString());
+  console.log(chunk);
 });
