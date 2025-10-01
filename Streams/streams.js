197f2e869a5d09cdcd3377c7d55662c551076bdc
Prisca <priscamasereli@gmail.com>
2025-10-01 09:38:43 +0200

    Feat: write a stream

--- Streams/streams.js
+++ Streams/streams.js
@@ -1,9 +1,15 @@
 const fs = require("fs");
+
 const readStream = fs.createReadStream("../File system/blog.txt", {
   encoding: "utf8",
 });
 
+const writeStream = fs.createWriteStream("../File system/blog2.txt");
+
 readStream.on("data", (chunk) => {
   console.log(".....NEW CHUNK.....");
   console.log(chunk);
+
+  writeStream.write("\nNEW CHUNK\n");
+  writeStream.write(chunk);
 });
