95841bbb87b3d09fea02c33e1ad1e15134182238
Prisca <priscamasereli@gmail.com>
2025-10-01 13:29:31 +0200

    Feat: Returning html pages

--- Creating Server/server.js
+++ Creating Server/server.js
@@ -1,13 +1,26 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
 
   //set header content type
-  res.setHeader("Content-Type", "text/plain"); // replace plain by html if you want to write an html text
+  res.setHeader("Content-Type", "text/html"); // replace plain by html if you want to write an html text
   //   res.write("<p>Hi prisca</p>");
-  res.write("hello, prisca");
-  res.end();
+  //   res.write("hello, prisca");
+  //   res.end();
+
+  // Returning HTML pages
+
+  fs.readFile("../views/index.html", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      //   res.write(data);
+      res.end(data); // you can pass it inside the end() and still does the same thing
+    }
+  });
 });
 
 server.listen(3000, "localhost", () => {
