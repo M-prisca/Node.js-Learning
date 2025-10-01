5bc2e84e238d3b2e9e44c59157710aaad3cdafc2
Prisca <priscamasereli@gmail.com>
2025-10-01 13:14:02 +0200

    Feat: Response object

--- Creating Server/server.js
+++ Creating Server/server.js
@@ -2,6 +2,12 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
+
+  //set header content type
+  res.setHeader("Content-Type", "text/plain"); // replace plain by html if you want to write an html text
+  //   res.write("<p>Hi prisca</p>");
+  res.write("hello, prisca");
+  res.end();
 });
 
 server.listen(3000, "localhost", () => {
